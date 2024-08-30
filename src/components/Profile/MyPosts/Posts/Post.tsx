fdcc56e749e792f466721d36e06fa91eb2b7b86f
SU-30SM <131552529+Artemka9231@users.noreply.github.com>
2024-08-30 15:14:13 +0300

    work 16

--- src/components/Profile/MyPosts/Posts/Post.tsx
+++ src/components/Profile/MyPosts/Posts/Post.tsx
@@ -3,12 +3,13 @@ import React from "react";
 import s from './Post.module.css'
 
 
-export const Post = () => {
+export const Post = (props:any) => {
+
     return (
 
                     <div className={s.item}>
                         <img src="https://lumiere-a.akamaihd.net/v1/images/a_avatarpandorapedia_jakesully_16x9_1098_02_b13c4171.jpeg?region=340%2C0%2C1081%2C1081" alt=""/>
-                        post1
+                        {props.message}
                         <div>
                             <span>like</span>
                         </div>
